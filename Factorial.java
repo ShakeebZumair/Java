d0e1a9d556c8abe8531587c1dead440e2914d4d6
ShakeebZumair <105355093+ShakeebZumair@users.noreply.github.com>
2022-05-11 16:33:18 +0530

    hello

---
+++ Factorial.java
@@ -0,0 +1,23 @@
+//Design a method int factorial (int no )which return int value to the method .
+//Print the result into main method (ex enter no as 5 ,factorial means  5*4*3*2*1 then the output is 120)
+
+
+
+public class Factorial {
+	
+	public static void main(String [] args ) {
+		//int a=5;
+		int b=1;
+		 for (int i=5 ; i>0; i--) {
+			 b=b*i; 
+		
+				
+		 } 
+		 System.out.println(b);
+		 
+		 
+		 }
+			 
+	}
+
+
